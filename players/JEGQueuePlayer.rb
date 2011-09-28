0cd15da30936ed553d99091014a6f3c5f5c87598
Matt Snyder <matt@cranialpulse.com>
2011-09-27 20:18:41 -0400

    Initalized repository with code from Ruby Quiz

---
+++ players/JEGQueuePlayer.rb
@@ -0,0 +1,20 @@
+class JEGQueuePlayer < Player
+ QUEUE = [ :rock,
+           :scissors,
+           :scissors ]
+
+  def initialize( opponent )
+    super
+
+    @index = 0
+  end
+
+  def choose
+    choice = QUEUE[@index]
+
+    @index += 1
+    @index = 0 if @index == QUEUE.size
+
+    choice
+  end
+end
